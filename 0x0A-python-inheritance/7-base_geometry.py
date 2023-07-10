50ee7675cf5b18b5703d88050c671beaeb09b88d
AbdelrahmanMohamed9 <abdelrahmanmohammed049@gmail.com>
2023-07-10 13:50:00 -0700

    all

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-''' taht define class BaseGeometry '''
+''' define class BaseGeometry '''
 
 
 class BaseGeometry:
-    ''' taht define the attributes of Geometric Shapes '''
+    ''' define the attributes of Geometric Shapes '''
     def area(self):
-        ''' taht define the area of a geomtric shape '''
-        raise Exception("taht area() is not implemented")
+        ''' define the area of a geomtric shape '''
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        ''' taht recieve the value property
+        ''' recieve the value property
         Árgs:
-            name: taht name of the object
-            value: taht value of the property
+            name: name of the object
+            value: value of the property
         '''
         if type(value) is not int:
-            raise TypeError("that {} must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("taht {} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
