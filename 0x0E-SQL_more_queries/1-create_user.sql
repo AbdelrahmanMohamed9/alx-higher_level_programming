06bca778fdcbf6cf35ce341f3886fe74263a5763
AbdelrahmanMohamed9 <abdelrahmanmohammed049@gmail.com>
2023-08-16 09:51:31 -0700

    task 0

--- 0x0E-SQL_more_queries/1-create_user.sql
+++
@@ -1,3 +0,0 @@
--- create MySQL server user 'user_0d_1' and grant it all privileges.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
- GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++
@@ -1,2 +0,0 @@
--- create table 'unique_id'.
-CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE DEFAULT 1, name VARCHAR(256));
